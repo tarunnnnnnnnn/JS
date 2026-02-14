4c007cf8014e49a4ef63af8c4b46bead77d8c8c3
Tarun Swami <152185804+tarunnnnnnnnn@users.noreply.github.com>
2026-02-14 14:30:50 +0530

    file17.js

---
+++ file17.js
@@ -0,0 +1,23 @@
+const products = ["product1", "product2", "product3"];
+// for(let i=0;i<products.length;i++){
+//     console.log(products[i])
+// }
+// products.forEach(()=>{})
+products.forEach((product) => console.log(product));
+
+// function controller(f) {
+//   console.log(f)
+// }
+// controller(10);
+
+// let a = 10
+// a//()
+// let name = "James"
+// //name()
+// const f1 = () => console.log("Hello World")
+// f1()
+
+// function greet(f){
+//     f()
+// }
+// greet(() => console.log("Hello World"))
