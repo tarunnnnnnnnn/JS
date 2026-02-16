5fc7c0b45f0df808a6879348cbc1dbaa1219e35a
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-02-16 11:34:40 +0530

    file37.js
    string methods

---
+++ file37.js
@@ -0,0 +1,11 @@
+let str = "Hello"   // string methods
+console.log(str.toUpperCase());// print that in upper case
+console.log(str.toLowerCase());// prints that in  lower case
+
+console.log(str.indexOf("l"));
+console.log(str.lastIndexOf("l"));
+console.log(str.includes("W")); //most used 
+console.log(str.startsWith("H"));
+console.log(str.endsWith("D"));
+console.log(str.slice(1,3));// both same ans
+console.log(str.substring(1,3)); // both same ans  but usually use substring
