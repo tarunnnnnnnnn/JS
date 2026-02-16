4c68ce8d4ac6134eeaad6f7ff2783cd08228c46e
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-02-16 11:27:43 +0530

    file36.js
    Math module functions applications

---
+++ file36.js
@@ -0,0 +1,15 @@
+let num = 10.759   // Math module
+
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+
+// console.log(num.toFixed(2)); // upto that num after decimal will be printed
+
+// console.log(isNaN(num)); // checking isnot a number so here it gives false
+
+// console.log(Math.random());// prints the random number
+// console.log(Math.max(5,1,7,8,3));// prints max that is greatest number of these out of these 
+// console.log(Math.min(5,1,7,8,3));// prints min that is smallest number of these out of these 
+
+console.log(Math.pow(2,3));
