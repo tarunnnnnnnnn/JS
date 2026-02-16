47faf21b2520007b419c0c285e2045f5804f9dda
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-02-16 11:40:41 +0530

    file38.js
    date methods functions imp most used:  new Date() and its methods

---
+++ file38.js
@@ -0,0 +1,14 @@
+let d = new Date()// used most
+console.log(d);              // date methods
+//get methods
+console.log(d.getDate());
+console.log(d.getMonth());
+console.log(d.getFullYear());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getTime());// it will print with serial number
+console.log(d.getMilliseconds());
+console.log(d.getDay());
+console.log(d.getTime());
+console.log(Date.now());
