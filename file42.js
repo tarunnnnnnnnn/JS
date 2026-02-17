03d4fa94a1e90a3619f7df254d0b4df85dc983c2
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-02-17 08:58:16 +0530

    Create file42.js

---
+++ file42.js
@@ -0,0 +1,14 @@
+function f1() {
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("This is f1 function")
+        resolve()
+    },3000)
+  });
+}
+function f2() {
+  console.log("This is f2 function");
+}
+f1()
+  .then(() => f2())
+  .catch((err) => console.log(err));
