f0a5919f12d4de5f94ff21ce632c14b7363b3b8d
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-02-18 12:46:47 +0530

    Update file58.js

--- file58.js
+++ file58.js
@@ -1,7 +1,28 @@
-const employee = {
-  name: "Amit",
-  greet() {
-    console.log(this.name);//this method uses
-  },
-};
-employee.greet();
+// const employee = {
+//   name: "Amit",
+//   greet() {
+//     console.log(this.name);
+//   },
+// };
+// employee.greet();
+
+
+// all these run without error without error in js when use strict is not used ok :)
+// "use strict"
+
+// // x = 10  //error
+// // console.log(x);
+
+// let x = 10     // no error
+// console.log(x);
+
+// "use strict"
+// // function add(a,a){
+// //     console.log(a+a); // error no same parameters in use strict 
+// // }
+
+// function add(a,b){
+//     console.log(a+b); // no error
+// }
+
+//////////////// use strict not allows 010 type numbers //unizeroes
